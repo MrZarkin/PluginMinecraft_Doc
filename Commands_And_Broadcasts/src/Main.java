a30898e2eaae4f9b0638896612e74873a44bedee
MrZarkin <matryck@outlook.fr>
2025-09-29 00:23:22 +0200

    First commit : Commands & Broadcasts

---
+++ Commands_And_Broadcasts/src/Main.java
@@ -0,0 +1,18 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		System.out.println("Le plugin viens de s'allumer");
+		getCommand("test").setExecutor(new CommandLoader());
+		getCommand("hello").setExecutor(new CommandLoader());
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		System.out.println("Le plugin viens de d'éteindre");
+	}
+}
